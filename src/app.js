0ba845d21043d04c2d452671dd12ad91682c7b8a
ParkEunwoo <dmsdn960@gmail.com>
2020-04-21 16:08:36 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,137 @@
+const name = prompt("닉네임을 설정하세요");
+document.querySelector(".name").textContent = name;
+const socket = io();
+socket.emit("join", name);
+const form = document.querySelector("#chat");
+const send = document.querySelector("#m");
+const messages = document.querySelector(".messages");
+const participants = document.querySelector(".participants");
+const selection = document.querySelector(".selection");
+const histories = document.querySelector(".history");
+const votes = document.querySelector(".votes");
+const stages = document.querySelector(".stages");
+let whatVote = "vote";
+let myNum = 0;
+let isKing = false;
+let myClass = "";
+
+socket.on("join", function (players) {
+  if (myNum === 0) myNum = players.length;
+  participants.innerHTML = players
+    .map((name, index) => `<li class="player${index + 1}">${name}</li>`)
+    .join("");
+});
+
+participants.addEventListener("click", (event) => {
+  if (isKing) {
+    socket.emit("member", event.target.classList[0].substr(-1));
+  }
+});
+
+selection.addEventListener("click", (event) => {
+  socket.emit(whatVote, myNum, event.target.classList[0]);
+  whatVote = "vote";
+  selection.style.display = "none";
+});
+
+socket.on("expedition", (list) => {
+  [...participants.children].forEach((elem, index) => {
+    if (list[index]) {
+      elem.classList.add("expedition");
+    } else {
+      elem.classList.remove("expedition");
+    }
+  });
+});
+
+socket.on("vote", (voteNum) => {
+  selection.style.display = "flex";
+  [...votes.children].forEach((elem, index) => {
+    if (index == voteNum) {
+      elem.classList.add("flag");
+    } else {
+      elem.classList.remove("flag");
+    }
+  });
+});
+
+socket.on("voteResult", (history) => {
+  const list = document.createElement("li");
+  list.innerHTML = history
+    .map((v) => `<div class="${v ? "agree" : "disagree"}"></div>`)
+    .join("");
+  histories.appendChild(list);
+});
+
+socket.on("expeditionResult", (stage, result) => {
+  stages.children[stage].classList.add(result ? "success" : "fail");
+});
+
+socket.on("startExpediton", (member) => {
+  if (member[myNum - 1]) {
+    selection.style.display = "flex";
+    whatVote = "expedition";
+  }
+});
+
+socket.on("king", (king) => {
+  if (myNum - 1 === king) isKing = true;
+  else isKing = false;
+  [...participants.children].forEach((elem, index) => {
+    if (king === index) {
+      elem.classList.add("king");
+    } else {
+      elem.classList.remove("king");
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (send.value === "") return;
+  socket.emit("chat message", name, send.value);
+  send.value = "";
+});
+
+socket.on("class", (shuffled) => {
+  myClass = shuffled[myNum - 1];
+  document.querySelector(".class").textContent = myClass;
+
+  switch (myClass) {
+    case "멀린":
+      [...participants.children].forEach((elem, index) => {
+        if (shuffled[index] == "암살자" || shuffled[index] == "모르가나") {
+          elem.classList.add("red");
+        }
+      });
+      break;
+    case "암살자":
+      [...participants.children].forEach((elem, index) => {
+        if (shuffled[index] == "암살자" || shuffled[index] == "모르가나") {
+          elem.classList.add("red");
+        }
+      });
+      break;
+    case "퍼시벌":
+      [...participants.children].forEach((elem, index) => {
+        if (shuffled[index] == "멀린" || shuffled[index] == "모르가나") {
+          elem.classList.add("blue");
+        }
+      });
+      break;
+    case "모르가나":
+      [...participants.children].forEach((elem, index) => {
+        if (shuffled[index] == "암살자" || shuffled[index] == "모르가나") {
+          elem.classList.add("red");
+        }
+      });
+      break;
+  }
+});
+
+socket.on("chat message", function (name, number, msg) {
+  const message = document.createElement("li");
+  message.innerHTML = `<span class="player${number}">${name}</span><span> : ${msg}</span>`;
+  messages.appendChild(message);
+  messages.scrollTop = messages.scrollHeight;
+});
---
+++ src/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const playerNum = 6;
+
+const players = [];
+const getNumber = {};
+const isReady = {};
+const stage = [2, 3, 2, 3, 3];
+let expeditionResult = [];
+let nowStage = 0;
+let expeditionNum = 0;
+const classes = ["멀린", "암살자", "모르가나", "퍼시벌", "시민", "시민"];
+
+let expedition = Array(playerNum).fill(false);
+let history = Array(playerNum);
+let votes = Array(playerNum).fill(false);
+let king = 0;
+let voteCnt = 0;
+
+app.use(express.static("src"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("join", (name) => {
+    players.push(name);
+    getNumber[name] = players.length;
+    isReady[name] = false;
+    console.log(name + " joined server");
+    io.emit("join", players);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("expedition", (num, value) => {
+    expeditionResult.push(value == "true");
+    if (expeditionResult.length == stage[nowStage]) {
+      const result = expeditionResult.every((v) => v);
+      io.emit("expeditionResult", nowStage, result);
+      expeditionResult = [];
+      nowStage++;
+      expedition = Array(playerNum).fill(false);
+      voteCnt = 0;
+      io.emit("expedition", expedition);
+      io.emit("king", king);
+    }
+  });
+  socket.on("member", (number) => {
+    if (expeditionNum < stage[nowStage]) {
+      if (expedition[number - 1]) return;
+      expeditionNum++;
+      expedition[number - 1] = true;
+      io.emit("expedition", expedition);
+      if (expeditionNum == stage[nowStage]) {
+        history = Array(playerNum);
+        io.emit("vote", voteCnt);
+      }
+    }
+  });
+  socket.on("vote", (num, value) => {
+    history[num - 1] = value == "true";
+    votes[num - 1] = true;
+    if (votes.every((v) => v)) {
+      king = (king + 1) % 6;
+      expeditionNum = 0;
+      votes = Array(6).fill(false);
+      io.emit("voteResult", history);
+      const isGo = history.reduce((a, c) => (c ? a + 1 : a), 0);
+      console.log(expedition);
+      console.log(isGo);
+      if (isGo > 2) {
+        io.emit("startExpediton", expedition);
+      } else {
+        history = Array(playerNum);
+        expedition = Array(playerNum).fill(false);
+        io.emit("vote", ++voteCnt);
+        io.emit("expedition", expedition);
+        io.emit("king", king);
+      }
+    }
+  });
+
+  socket.on("chat message", (name, msg) => {
+    console.log(`${name} : ${msg}`);
+    io.emit("chat message", name, getNumber[name], msg);
+    if (msg === "ready") {
+      isReady[name] = true;
+      if (Object.values(isReady).every((v) => v)) {
+        const shuffled = classes.sort(() => 0.5 - Math.random());
+        io.emit("class", shuffled);
+        console.log(shuffled);
+        console.log("start");
+        io.emit("chat message", "관리자", 0, "게임이 시작되었습니다.");
+        io.emit("king", king);
+      }
+    }
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
